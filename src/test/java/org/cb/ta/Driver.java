8fce10e537a671d5170f174a22e2fe472b21e01d
kutluhang <kutluhang@yahoo.com>
2021-01-09 20:27:36 +0100

    Iniit

---
+++ src/test/java/org/cb/ta/Driver.java
@@ -0,0 +1,11 @@
+package org.cb.ta;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public abstract class Driver {
+    public static WebDriver getDriver() {
+        System.setProperty("webdriver.chrome.driver","webdriver/chromedriver");
+        return new ChromeDriver();
+    }
+}
---
+++ src/test/java/org/cb/ta/WDUHomeTest.java
@@ -0,0 +1,37 @@
+package org.cb.ta;
+
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+public class WDUHomeTest extends BaseTest {
+
+    private final WebElement dataTablesButtonStatesLinkElement;
+    private final WDUHome wduHome;
+
+    public WDUHomeTest() {
+        super();
+        wduHome = new WDUHome(driver);
+        dataTablesButtonStatesLinkElement = wduHome.getDataTablesButtonStatesLink();
+    }
+
+    @Test
+    public void checkIfDataTablesButtonStatesLinkExists() {
+        assertNotNull(dataTablesButtonStatesLinkElement);
+    }
+
+    @Test
+    public void checkIfDataTablesButtonStatesLinkIsEnabled() {
+        assertTrue(dataTablesButtonStatesLinkElement.isEnabled());
+    }
+
+    @Test
+    public void clickDataTablesButtonStatesLink() {
+        dataTablesButtonStatesLinkElement.click();
+    }
+
+
+
+}
